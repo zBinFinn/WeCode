8a064d1fdc4e67c7bc274f26afa710a1c5071930
zBinFinn <84500718+zBinFinn@users.noreply.github.com>
2025-03-07 21:59:54 +0100

    feat: Keybind to pin item lore to the top right of the screen

---
+++ src/main/java/org/zbinfinn/wecode/features/PinItemKeybindFeature.java
@@ -0,0 +1,71 @@
+package org.zbinfinn.wecode.features;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import org.zbinfinn.wecode.GUIKeyBinding;
+import org.zbinfinn.wecode.WeCode;
+
+import java.util.List;
+
+public class PinItemKeybindFeature extends Feature {
+    private final GUIKeyBinding pinKeyBind = new GUIKeyBinding(
+            "key.wecode.pinitem",
+            InputUtil.Type.KEYSYM,
+            InputUtil.GLFW_KEY_RIGHT_ALT,
+            "key.wecode.category"
+    );
+    private final GUIKeyBinding unpinKeyBind = new GUIKeyBinding(
+            "key.wecode.unpinitem",
+            InputUtil.Type.KEYSYM,
+            InputUtil.GLFW_KEY_O,
+            "key.wecode.category"
+    );
+
+    private ItemStack pinnedItem;
+
+    @Override
+    public void activate() {
+        KeyBindingHelper.registerKeyBinding(pinKeyBind);
+        KeyBindingHelper.registerKeyBinding(unpinKeyBind);
+    }
+
+    @Override
+    public void tick() {
+        if (!unpinKeyBind.isPressed()) {
+            return;
+        }
+        pinnedItem = null;
+    }
+
+    @Override
+    public void tooltip(ItemStack item, Item.TooltipContext tooltipContext, TooltipType tooltipType, List<Text> list) {
+        if (!pinKeyBind.isPressed()) {
+            return;
+        }
+        if (pinnedItem != null && pinnedItem.equals(item)) {
+            return;
+        }
+
+        pinnedItem = item.copy();
+    }
+
+    @Override
+    public void hudRender(DrawContext draw, RenderTickCounter tickCounter) {
+        if (pinnedItem == null) {
+            return;
+        }
+        MatrixStack stack = draw.getMatrices();
+
+        stack.push();
+        stack.translate(0, 0, 5000);
+        draw.drawItemTooltip(WeCode.MC.textRenderer, pinnedItem, draw.getScaledWindowWidth(), 20);
+        stack.pop();
+    }
+}
